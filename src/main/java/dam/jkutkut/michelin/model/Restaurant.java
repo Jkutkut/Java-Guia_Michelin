1854ad860cdacc0cfa6a6b21334bc6737a0ce30d
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-12 12:13:57 +0200

    working on the restaurant class

---
+++ src/main/java/dam/jkutkut/michelin/model/Restaurant.java
@@ -0,0 +1,27 @@
+package dam.jkutkut.michelin.model;
+
+public class Restaurant {
+    public static final String[] REGIONS = {
+        "Andalucia",
+        "Aragon",
+        "Asturias",
+        "Baleares",
+        "Canarias",
+        "Cantabria",
+        "Castilla-La-Mancha",
+        "Castilla-y-Leon",
+        "Cataluna",
+        "Ceuta",
+        "Ciudad-Real",
+        "Comunidad-Valenciana",
+        "Extremadura",
+        "Galicia",
+        "La-Rioja",
+        "Madrid",
+        "Murcia",
+        "Navarra",
+        "Pais-Vasco",
+        "Valencia"
+    };
+
+}
