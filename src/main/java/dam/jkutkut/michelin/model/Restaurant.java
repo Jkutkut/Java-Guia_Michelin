d46119f070a58c67eceff0347e348ba44adcf459
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-12 14:55:08 +0200

    Restaurant now with logic to validate input

--- src/main/java/dam/jkutkut/michelin/model/Restaurant.java
+++ src/main/java/dam/jkutkut/michelin/model/Restaurant.java
@@ -1,5 +1,7 @@
 package dam.jkutkut.michelin.model;
 
+import dam.jkutkut.exception.InvalidDataException;
+
 public class Restaurant {
     public static final String[] REGIONS = {
         "Andalucia",
@@ -42,6 +44,152 @@ public class Restaurant {
     public static final int MIN_DISTINCTION = 1;
     public static final int MAX_DISTINCTION = 3;
 
+    private static final double NULL_PRICE = -1;
 
+    private static final String PHONE_REGEX = "^(\\+\\d{2,3})? ?\\d{3} ?\\d{3} ?\\d{3}$";
+    private static final String WEB_REGEX = "(http[s]?:\\/\\/)?(ww[w2]\\.)?(([a-zA-Z0-9\\-]+\\.)+([a-zA-Z\\-])+)";
 
+    private String name;
+    private String region;
+    private String type;
+    private int distinction;
+    private double minPrice;
+    private double maxPrice;
+    private String phone;
+    private String web;
+
+    public Restaurant() {
+        this.name = null;
+        this.region = null;
+        this.type = null;
+        this.distinction = 0;
+        this.minPrice = NULL_PRICE;
+        this.maxPrice = NULL_PRICE;
+        this.phone = null;
+        this.web = null;
+    }
+
+    public Restaurant(String name, String region, String type, int distinction, double minPrice, double maxPrice, String phone, String web) {
+        this.setName(name);
+        this.setRegion(region);
+        this.setType(type);
+        this.setDistinction(distinction);
+        this.setMinPrice(minPrice);
+        this.setMaxPrice(maxPrice);
+        this.setPhone(phone);
+        this.setWeb(web);
+
+    }
+
+    // GETTERS
+    public void validate() throws InvalidDataException {
+        if (!isValidName(this.name))
+            throw new InvalidDataException("Invalid name");
+        if (!isValidRegion(this.region))
+            throw new InvalidDataException("Invalid region");
+        if (!isValidType(this.type))
+            throw new InvalidDataException("Invalid type");
+        if (!isValidDistinction(this.distinction))
+            throw new InvalidDataException("Invalid distinction");
+        if (!isValidMinPrice(this.minPrice, this.maxPrice))
+            throw new InvalidDataException("Invalid min price");
+        if (!isValidMaxPrice(this.maxPrice, this.minPrice))
+            throw new InvalidDataException("Invalid max price");
+        if (!isValidPhone(this.phone))
+            throw new InvalidDataException("Invalid phone");
+        if (!isValidWeb(this.web))
+            throw new InvalidDataException("Invalid web");
+    }
+
+    // SETTERS
+    public void setName(String name) {
+        if (!isValidName(name))
+            throw new InvalidDataException("Invalid name");
+        this.name = name;
+    }
+
+    public void setRegion(String region) {
+        if (!isValidRegion(region))
+            throw new InvalidDataException("Invalid region");
+        this.region = region;
+    }
+
+    public void setType(String type) {
+        if (!isValidType(type))
+            throw new InvalidDataException("Invalid type");
+        this.type = type;
+    }
+
+    public void setDistinction(int distinction) {
+        if (!isValidDistinction(distinction))
+            throw new InvalidDataException("Invalid distinction: value must be in range", MIN_DISTINCTION, MAX_DISTINCTION);
+        this.distinction = distinction;
+    }
+
+    public void setMinPrice(double minPrice) {
+        if (!isValidMinPrice(minPrice, this.maxPrice))
+            throw new InvalidDataException("Invalid min price");
+        this.minPrice = minPrice;
+    }
+
+    public void setMaxPrice(double maxPrice) {
+        if (!isValidMaxPrice(maxPrice, this.minPrice))
+            throw new InvalidDataException("Invalid max price");
+        this.maxPrice = maxPrice;
+    }
+
+    public void setPhone(String phone) {
+        if (!isValidPhone(phone))
+            throw new InvalidDataException("Invalid phone");
+        this.phone = phone;
+    }
+
+    public void setWeb(String web) {
+        if (!isValidWeb(web))
+            throw new InvalidDataException("Invalid web");
+        this.web = web;
+    }
+
+    // Checkers
+    public static boolean isValidName(String name) {
+        return name != null;
+    }
+
+    public static boolean isValidRegion(String region) {
+        if (region == null || region.isBlank())
+            return false;
+        for (String r : REGIONS)
+            if (r.equals(region))
+                return true;
+        return false;
+    }
+
+    public static boolean isValidType(String type) {
+        if (type == null || type.isBlank())
+            return false;
+        for (String t : TYPES)
+            if (t.equals(type))
+                return true;
+        return false;
+    }
+
+    public static boolean isValidDistinction(int distinction) {
+        return distinction >= MIN_DISTINCTION && distinction <= MAX_DISTINCTION;
+    }
+
+    public static boolean isValidMinPrice(double minPrice, double maxPrice) {
+        return minPrice > 0 && (maxPrice != NULL_PRICE && minPrice <= maxPrice);
+    }
+
+    public static boolean isValidMaxPrice(double maxPrice, double minPrice) {
+        return maxPrice > 0 && (minPrice != NULL_PRICE && maxPrice >= minPrice);
+    }
+
+    public static boolean isValidPhone(String phone) {
+        return phone != null && phone.matches(PHONE_REGEX);
+    }
+
+    public static boolean isValidWeb(String web) {
+        return web != null && web.matches(WEB_REGEX);
+    }
 }
