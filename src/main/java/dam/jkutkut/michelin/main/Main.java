1057214822b5366f0423e319c47fc53f484c6c45
Jkutkut <jorgeregonzalez@gmail.com>
2022-05-12 09:57:43 +0200

    working on basic proyect structure with initial docs

---
+++ src/main/java/dam/jkutkut/michelin/main/Main.java
@@ -0,0 +1,18 @@
+package dam.jkutkut.michelin.main;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+//                Login vLogin = new Login();
+
+//                Controller controller = new Controller(vLogin, vSignUp, loginDB);
+//                vLogin.setController(controller);
+//                vLogin.setVisible(true);
+            }
+        });
+    }
+}
